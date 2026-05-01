78a59466bc8553d45ba9cd4484d7bfa5c83d9a9b
uka yura <uka@ukas-MacBook-Pro.local>
2026-05-01 21:23:59 +0800

    bug fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './products/product.module';
 import { OrderModule } from './order/order.module';
 import { FilesModule } from './files/files.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule, OrderModule, FilesModule],
+  imports: [
+    PrismaModule,
+    ProductsModule,
+    CategoryModule,
+    OrderModule,
+    FilesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
